29eba60aa87fc33213f3a8a04db0a068e8aa7c98
十火 <wangyanwen.wyw@oceanbase.com>
2025-06-27 22:24:10 +0800

    update test

--- test/hello_world.c
+++ test/hello_world.c
@@ -1,5 +1,48 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <netinet/ip.h>
+#include <netinet/if_ether.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define PORT 8081
+#define BUFFER_SIZE 1048576
 
 int main() {
-    printf("Hello World");
+    int server_fd, client_fd;
+    struct sockaddr_in addr;
+    char buffer[BUFFER_SIZE];
+
+    /**
+     * Socket initialization
+     */
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY; 
+    addr.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 5) < 0) {
+        perror("listen failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d...\n", PORT);
 }
