6f7be7c91f39fb6317200160a490956fb8c4bd71
十火 <wangyanwen.wyw@oceanbase.com>
2025-06-27 23:36:54 +0800

    feat: increase stack-limit of server thread

--- src/main.c
+++ src/main.c
@@ -22,11 +22,16 @@ int main(int argc, char* argv[]) {
     pthread_t sniffer_tid;
     pthread_t server_tid;
 
+    pthread_attr_t st_attr;
+    pthread_attr_init(&st_attr);
+    pthread_attr_setstacksize(&st_attr, 1048576 * 2);
+
     pthread_create(&sniffer_tid, NULL, sniffing, (char*)dev);
-    pthread_create(&server_tid, NULL, server_handler, NULL);
+    pthread_create(&server_tid, &st_attr, server_handler, NULL);
 
     pthread_join(sniffer_tid, NULL);
     pthread_join(server_tid, NULL);
 
+    pthread_attr_destroy(&st_attr);
     return 0;
 }
